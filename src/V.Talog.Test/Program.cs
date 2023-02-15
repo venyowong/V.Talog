ea7ae82227308c46c3cc7e13bcb8527259243b59
Venyo Wong <venyowong@163.com>
2023-02-15 15:27:00 +0800

    test

--- src/V.Talog.Test/Program.cs
+++ src/V.Talog.Test/Program.cs
@@ -1,10 +1,11 @@
 ﻿using Bogus;
 using Serilog;
+using System.Diagnostics;
 using V.Talog;
 using V.Talog.Client;
 using V.Talog.Extension.Serilog;
 
-V.Talog.Client.Config.TalogServer = "https://vbranch.cn/talog";
+//V.Talog.Client.Config.TalogServer = "https://vbranch.cn/talog";
 Log.Logger = new LoggerConfiguration()
     .Enrich.WithThreadId()
     .MinimumLevel.Information()
@@ -14,30 +15,52 @@ Log.Logger = new LoggerConfiguration()
 
 using var talogger = new Talogger();
 var logs = new Faker<V.Talog.Test.Log>()
-    .RuleFor(l => l.Time, f => f.Date.Past())
+    .RuleFor(l => l.Time, f => f.Date.Between(DateTime.Now.AddDays(-5), DateTime.Now))
     .RuleFor(l => l.Level, f => f.Random.Int(0, 2))
     .RuleFor(l => l.IP, f => f.Internet.Ip())
     .RuleFor(l => l.UserId, f => f.Random.Guid().ToString())
     .RuleFor(l => l.Message, f => f.Random.Words());
 
-foreach (var i in Enumerable.Range(0, 20))
-{
-    var log = logs.Generate();
-    Log.Information($"{log.Time} [{log.Level}] {log.IP} {log.Message}");
-    Log.Warning(new Exception(log.Message), $"{log.Time} [{log.Level}] {log.IP} {log.Message}");
-}
-
 //foreach (var i in Enumerable.Range(0, 20))
 //{
 //    var log = logs.Generate();
-//    talogger.CreateHeaderIndexer("log3")
-//        .Tag("date", log.Time.Date.ToString())
-//        .Tag("level", log.Level.ToString())
-//        .Tag("ip", log.IP)
-//        .Data($@"{log.Time} [{log.Level}] {log.IP} 
-//            {log.Message}")
-//        .Save();
+//    Log.Information($"{log.Time} [{log.Level}] {log.IP} {log.Message}");
+//    Log.Warning(new Exception(log.Message), $"{log.Time} [{log.Level}] {log.IP} {log.Message}");
 //}
+
+var stopwatch = new Stopwatch();
+var times = 200000;
+//var logList = Enumerable.Range(0, times)
+//    .AsParallel()
+//    .Select(x => logs.Generate())
+//    .ToList();
+
+//stopwatch.Start();
+//logList.AsParallel()
+//    .ForAll(log =>
+//    {
+//        talogger.CreateHeaderIndexer("log3")
+//            .Tag("date", log.Time.Date.ToString())
+//            .Tag("level", log.Level.ToString())
+//            .Data($@"{log.Time} [{log.Level}] {log.IP} 
+//                    {log.Message}")
+//            .Save();
+//    });
+//stopwatch.Stop();
+foreach (var i in Enumerable.Range(0, times))
+{
+    var log = logs.Generate();
+    stopwatch.Start();
+    talogger.CreateHeaderIndexer("log3")
+        .Tag("date", log.Time.Date.ToString())
+        .Tag("level", log.Level.ToString())
+        .Data($@"{log.Time} [{log.Level}] {log.IP} 
+            {log.Message}")
+        .Save();
+    stopwatch.Stop();
+}
+Console.WriteLine($"索引 {times} 条日志，总耗时：{stopwatch.Elapsed}");
+
 //foreach (var i in Enumerable.Range(0, 20))
 //{
 //    var log = logs.Generate();
